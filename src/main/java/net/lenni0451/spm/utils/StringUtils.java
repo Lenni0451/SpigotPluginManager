6d19093d34c3f05e72cff37c543e32c0d9e45d71
Lenni0451 <lenni0451@laptop>
2021-05-16 19:19:56 +0200

    Fixed bug in StringUtils

--- src/main/java/net/lenni0451/spm/utils/StringUtils.java
+++ src/main/java/net/lenni0451/spm/utils/StringUtils.java
@@ -6,13 +6,13 @@ public class StringUtils {
 
     public static String arrayToString(final String[] array) {
         StringBuilder out = new StringBuilder();
-        for (String s : array) out.append(s.isEmpty() ? "" : ", ").append(s);
+        for (String s : array) out.append((out.length() == 0) ? "" : ", ").append(s);
         return out.toString();
     }
 
     public static String listToString(final List<String> list) {
         StringBuilder out = new StringBuilder();
-        for (String s : list) out.append(s.isEmpty() ? "" : ", ").append(s);
+        for (String s : list) out.append((out.length() == 0) ? "" : ", ").append(s);
         return out.toString();
     }
 
