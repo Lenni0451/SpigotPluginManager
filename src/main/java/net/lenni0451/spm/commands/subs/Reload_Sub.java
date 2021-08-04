f6b541c8b5c23cf07f2188f97f7bff4168dd28f5
Lenni0451 <lenni0451@laptop>
2021-08-04 13:31:27 +0200

    Added translations to Reload_Sub

--- src/main/java/net/lenni0451/spm/commands/subs/Reload_Sub.java
+++ src/main/java/net/lenni0451/spm/commands/subs/Reload_Sub.java
@@ -2,11 +2,13 @@ package net.lenni0451.spm.commands.subs;
 
 import net.lenni0451.spm.PluginManager;
 import net.lenni0451.spm.commands.subs.types.ISubCommand;
+import net.lenni0451.spm.utils.I18n;
 import net.lenni0451.spm.utils.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +23,7 @@ public class Reload_Sub implements ISubCommand {
         } else {
             Optional<Plugin> plugin = PluginManager.getInstance().getPluginUtils().getPlugin(args[0]);
             if (!plugin.isPresent()) {
-                Logger.sendPrefixMessage(sender, "§cThe plugin could not be found.");
+                Logger.sendPrefixMessage(sender, I18n.t("pm.general.pluginNotFound"));
                 return true;
             }
 
@@ -29,17 +31,17 @@ public class Reload_Sub implements ISubCommand {
                 PluginManager.getInstance().getPluginUtils().unloadPlugin(plugin.get());
             } catch (Throwable e) {
                 e.printStackTrace();
-                Logger.sendPrefixMessage(sender, "§cCould not unload the plugin §6" + plugin.get().getName() + "§c." + (e.getMessage() != null ? (" §7(" + e.getMessage() + ")") : ""));
+                Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.reload.unloadError", plugin.get().getName(), e.getMessage() == null ? I18n.t("pm.general.checkConsole") : e.getMessage()));
                 return true;
             }
             try {
                 PluginManager.getInstance().getPluginUtils().loadPlugin(plugin.get());
             } catch (Throwable e) {
                 e.printStackTrace();
-                Logger.sendPrefixMessage(sender, "§cCould not load the plugin §6" + plugin.get().getName() + "§c." + (e.getMessage() != null ? (" §7(" + e.getMessage() + ")") : ""));
+                Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.reload.loadError", plugin.get().getName(), e.getMessage() == null ? I18n.t("pm.general.checkConsole") : e.getMessage()));
                 return true;
             }
-            Logger.sendPrefixMessage(sender, "§aThe plugin §6" + plugin.get().getName() + " §ahas been reloaded.");
+            Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.reload.success", plugin.get().getName()));
         }
 
         return true;
@@ -61,9 +63,7 @@ public class Reload_Sub implements ISubCommand {
 
     @Override
     public void getHelp(List<String> lines) {
-        lines.add("Unload and load a plugin again to release");
-        lines.add("all its loaded resources and update it");
-        lines.add("during runtime or try to fix some issues.");
+        Collections.addAll(lines, I18n.mt("pm.subcommands.reload.help"));
     }
 
 }
