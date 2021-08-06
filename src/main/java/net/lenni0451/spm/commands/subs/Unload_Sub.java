fa11084c65d9cff9f0ef4b211c5865c67126a475
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2021-08-06 10:54:30 +0200

    Added translations for Unload_Sub

--- src/main/java/net/lenni0451/spm/commands/subs/Unload_Sub.java
+++ src/main/java/net/lenni0451/spm/commands/subs/Unload_Sub.java
@@ -2,10 +2,12 @@ package net.lenni0451.spm.commands.subs;
 
 import net.lenni0451.spm.PluginManager;
 import net.lenni0451.spm.commands.subs.types.ISubCommand;
+import net.lenni0451.spm.utils.I18n;
 import net.lenni0451.spm.utils.Logger;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Unload_Sub implements ISubCommand {
@@ -24,21 +26,21 @@ public class Unload_Sub implements ISubCommand {
                 try {
                     PluginManager.getInstance().getPluginUtils().unloadPlugin(plugin);
                 } catch (Throwable e) {
-                    Logger.sendPrefixMessage(sender, "§cCould not unload the plugin §6" + plugin.getName() + "§c." + (e.getMessage() != null ? (" §7(" + e.getMessage() + ")") : ""));
+                    Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.unload.unloadError", plugin.getName(), e.getMessage() == null ? I18n.t("pm.general.checkConsole") : e.getMessage()));
                 }
             }
 
-            Logger.sendPrefixMessage(sender, "§aUnloaded all plugins §e(" + plugins.size() + ")§a.");
+            Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.unload.batchSuccess", plugins.size()));
         } else {
             if (!PluginManager.getInstance().getPluginUtils().getPlugin(args[0]).isPresent()) {
-                Logger.sendPrefixMessage(sender, "§cThe plugin could not be found.");
+                Logger.sendPrefixMessage(sender, I18n.t("pm.general.pluginNotFound"));
                 return true;
             }
             try {
                 PluginManager.getInstance().getPluginUtils().unloadPlugin(args[0]);
-                Logger.sendPrefixMessage(sender, "§aThe plugin has been unloaded.");
+                Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.unload.success"));
             } catch (Throwable e) {
-                Logger.sendPrefixMessage(sender, "§cThe plugin could not be unloaded." + (e.getMessage() != null ? (" §7(" + e.getMessage() + ")") : ""));
+                Logger.sendPrefixMessage(sender, I18n.t("pm.subcommands.unload.unloadError", args[0], e.getMessage() == null ? I18n.t("pm.general.checkConsole") : e.getMessage()));
             }
         }
 
@@ -61,9 +63,7 @@ public class Unload_Sub implements ISubCommand {
 
     @Override
     public void getHelp(List<String> lines) {
-        lines.add("Unload a plugin and release all its loaded resources.");
-        lines.add("If you are testing new plugins you can just unload");
-        lines.add("them again if they do not fit your needs.");
+        Collections.addAll(lines, I18n.mt("pm.subcommands.unload.help"));
     }
 
 }
