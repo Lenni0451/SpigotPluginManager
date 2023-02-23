35ad7aa822ebf7d516968785186ee39f091a9ca6
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-02-23 19:49:53 +0100

    Added base code to main

--- src/main/java/net/lenni0451/pluginmanager/Main.java
+++ src/main/java/net/lenni0451/pluginmanager/Main.java
@@ -1,6 +1,26 @@
 package net.lenni0451.pluginmanager;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+
+    public Main() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
 
 }
