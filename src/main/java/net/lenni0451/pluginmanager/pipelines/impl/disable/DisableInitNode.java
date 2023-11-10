15bc1453b6a64dbaed8b3c1db07602d71c6d4601
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-11-10 17:54:04 +0100

    Only enable/disable if required

--- src/main/java/net/lenni0451/pluginmanager/pipelines/impl/disable/DisableInitNode.java
+++ src/main/java/net/lenni0451/pluginmanager/pipelines/impl/disable/DisableInitNode.java
@@ -2,8 +2,10 @@ package net.lenni0451.pluginmanager.pipelines.impl.disable;
 
 import net.lenni0451.pluginmanager.pipelines.IPipelineNode;
 import net.lenni0451.pluginmanager.pipelines.PipelineContext;
+import org.bukkit.plugin.Plugin;
 
 import static net.lenni0451.pluginmanager.pipelines.PipelineConstants.DATA_NEXT_STEP;
+import static net.lenni0451.pluginmanager.pipelines.PipelineConstants.DATA_PLUGIN;
 import static net.lenni0451.pluginmanager.pipelines.impl.disable.DisablePipelineNodes.GET_PLUGIN_MANAGER;
 import static net.lenni0451.pluginmanager.pipelines.impl.disable.DisablePipelineNodes.INTERRUPT_THREADS;
 
@@ -12,8 +14,13 @@ public class DisableInitNode implements IPipelineNode<DisablePipelineNodes> {
     @Override
     public DisablePipelineNodes execute(PipelineContext context) {
         context.passAll();
-        context.pass(DATA_NEXT_STEP, INTERRUPT_THREADS);
-        return GET_PLUGIN_MANAGER;
+        Plugin plugin = context.consume(DATA_PLUGIN);
+        if (plugin.isEnabled()) {
+            context.pass(DATA_NEXT_STEP, INTERRUPT_THREADS);
+            return GET_PLUGIN_MANAGER;
+        } else {
+            return null;
+        }
     }
 
 }
--- src/main/java/net/lenni0451/pluginmanager/pipelines/impl/enable/EnableInitNode.java
+++ src/main/java/net/lenni0451/pluginmanager/pipelines/impl/enable/EnableInitNode.java
@@ -2,8 +2,10 @@ package net.lenni0451.pluginmanager.pipelines.impl.enable;
 
 import net.lenni0451.pluginmanager.pipelines.IPipelineNode;
 import net.lenni0451.pluginmanager.pipelines.PipelineContext;
+import org.bukkit.plugin.Plugin;
 
 import static net.lenni0451.pluginmanager.pipelines.PipelineConstants.DATA_NEXT_STEP;
+import static net.lenni0451.pluginmanager.pipelines.PipelineConstants.DATA_PLUGIN;
 import static net.lenni0451.pluginmanager.pipelines.impl.enable.EnablePipelineNodes.ENABLE_PLUGIN;
 import static net.lenni0451.pluginmanager.pipelines.impl.enable.EnablePipelineNodes.GET_PLUGIN_MANAGER;
 
@@ -12,8 +14,13 @@ public class EnableInitNode implements IPipelineNode<EnablePipelineNodes> {
     @Override
     public EnablePipelineNodes execute(PipelineContext context) {
         context.passAll();
-        context.pass(DATA_NEXT_STEP, ENABLE_PLUGIN);
-        return GET_PLUGIN_MANAGER;
+        Plugin plugin = context.consume(DATA_PLUGIN);
+        if (plugin.isEnabled()) {
+            return null;
+        } else {
+            context.pass(DATA_NEXT_STEP, ENABLE_PLUGIN);
+            return GET_PLUGIN_MANAGER;
+        }
     }
 
 }
