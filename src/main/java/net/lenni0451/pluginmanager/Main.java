2f6ec173c37185c3f275ec07e50cd681aaffb18d
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-11-10 17:25:53 +0100

    Load and update config

--- src/main/java/net/lenni0451/pluginmanager/Main.java
+++ src/main/java/net/lenni0451/pluginmanager/Main.java
@@ -1,9 +1,14 @@
 package net.lenni0451.pluginmanager;
 
+import com.tchristofferson.configupdater.ConfigUpdater;
+import net.lenni0451.pluginmanager.i18n.I18n;
 import net.lenni0451.pluginmanager.i18n.TranslationsConfig;
 import net.lenni0451.pluginmanager.pipelines.PipelineManager;
 import net.lenni0451.pluginmanager.ui.ScreenManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends JavaPlugin {
 
@@ -24,8 +29,30 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         TranslationsConfig.load();
+        if (!this.loadConfig()) {
+            this.setEnabled(false);
+            return;
+        }
+
         this.screenManager = new ScreenManager();
         this.pipelineManager = new PipelineManager();
+    }
+
+    private boolean loadConfig() {
+        File configFile = new File(this.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            this.saveDefaultConfig();
+        } else {
+            try {
+                ConfigUpdater.update(this, "config.yml", configFile);
+            } catch (IOException e) {
+                this.getLogger().severe(I18n.Config.FailedToLoad);
+                this.getLogger().severe(I18n.Config.TryDeletingConfig);
+                return false;
+            }
+            this.reloadConfig();
+        }
+        return true;
     }
 
     @Override
--- src/main/java/net/lenni0451/pluginmanager/i18n/I18n.java
+++ src/main/java/net/lenni0451/pluginmanager/i18n/I18n.java
@@ -2,5 +2,9 @@ package net.lenni0451.pluginmanager.i18n;
 
 public class I18n {
 
+    public static class Config {
+        public static String FailedToLoad = "Failed to save/update the config.yml. Please make sure the file is writable.";
+        public static String TryDeletingConfig = "If the error persists please delete the config file and restart the server.";
+    }
 
 }
